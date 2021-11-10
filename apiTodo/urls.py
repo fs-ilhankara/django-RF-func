7eb2d8b61cc70c7a2acd825f65060694b9cd9f52
mirackarakasli <ilhansamikara@gmail.com>
2021-11-10 21:42:06 +0300

    2.ders sonu

--- apiTodo/urls.py
+++ apiTodo/urls.py
@@ -1,16 +1,22 @@
 from django.urls import path
-from .views import home, todoList, todoListCreate, toDo_list, todoListUpdate, todoListDelete, toDo_Detail, TodoListe, TodoDetail
+from .views import home, TodoDetail,TodoListCreate, TodoRetrieveUpdateDelete, TodoConcListCreate, TodoConcListUpdateDestroy
+                    
+
 
 urlpatterns = [
-    
     path('', home),
-    path('todoList/', todoList),
-    path('todoListCreate/', todoListCreate),
-    path('toDo_list/', toDo_list),
-    path('todoListUpdate/<int:pk>', todoListUpdate),
-    path('todoListDelete/<int:pk>', todoListDelete),
-    path('toDo_Detail/<int:pk>', toDo_Detail),
-    path('TodoListe/', TodoListe.as_view()),
-    path('TodoDetail/<int:pk>', TodoDetail.as_view()),
-   
+    # path('todoList/', todoList),
+    # path('todoListCreate/', todoListCreate),
+    # path('toDo_list/', toDo_list),
+    # path('todoListUpdate/<int:pk>/', todoListUpdate),
+    # path('todoListDelete/<int:pk>/', todoListDelete),
+    # path('toDo_detail/<int:pk>/', toDo_detail),
+    # path('todo-list/', TodoList.as_view()),
+    path('todo-list/', TodoListCreate.as_view()),
+    path('todo-detail/<int:pk>', TodoDetail.as_view()),
+    path('todoupdate/<int:pk>', TodoRetrieveUpdateDelete.as_view()),
+    path('TodoConcListCreate/', TodoConcListCreate.as_view()),
+    path('TodoConcListUpdateDestroy/<int:pk>', TodoConcListUpdateDestroy.as_view()),
+
+
 ]
--- apiTodo/views.py
+++ apiTodo/views.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView, mixins, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 
 
@@ -128,7 +129,41 @@ class TodoDetail(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+#----------GENERIC API View --------------#
 
+class TodoListCreate(mixins.ListModelMixin, mixins.CreateModelMixin, GenericAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+
+class TodoRetrieveUpdateDelete(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, GenericAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
+class TodoConcListCreate(ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    
+class TodoConcListUpdateDestroy(RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
 
 
 
